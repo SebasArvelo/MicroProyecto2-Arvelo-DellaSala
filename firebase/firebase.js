6d9d116722b700274ca43fda608814d529efe9c0
SebasArvelo <s.arvelo@correo.unimet.edu.ve>
2024-03-11 21:53:28 -0400

    Update de HomePage y de la firebase

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,19 +1,18 @@
-// firebase/firebase.js
-
 import firebase from 'firebase/app';
-import 'firebase/auth';
 import 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDhPei_FK8Cttgcw0mI-5WpNIIyCumgktA",
   authDomain: "microproyecto-arvelo-dellasala.firebaseapp.com",
   projectId: "microproyecto-arvelo-dellasala",
-  // Agrega más configuraciones si es necesario
+  storageBucket: "microproyecto-arvelo-dellasala.appspot.com",
+  messagingSenderId: "326870847729",
+  appId: "1:326870847729:web:1d09c2d1465d4a389c1950"
 };
 
-// Inicializar Firebase
-const app = firebase.initializeApp(firebaseConfig);
-const firestore = firebaseApp.firestore();
+firebase.initializeApp(firebaseConfig);
 
-export { auth, firestore };
+export const firestore = firebase.firestore();
+
+export default firebase;
 
--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { firestore } from '../firebase/firebase';
 
 const HomePage = ({ user }) => {
+  const [videojuegos, setVideojuegos] = useState([]);
+  const [clubes, setClubes] = useState([]);
+
+  useEffect(() => {
+    const fetchVideojuegos = async () => {
+      const videojuegosCollection = await firestore.collection('videojuegos').get();
+      const videojuegosData = videojuegosCollection.docs.map(doc => doc.data());
+      setVideojuegos(videojuegosData);
+    };
+
+    const fetchClubes = async () => {
+      const clubesCollection = await firestore.collection('clubes').get();
+      const clubesData = clubesCollection.docs.map(doc => doc.data());
+      setClubes(clubesData);
+    };
+
+    fetchVideojuegos();
+    fetchClubes();
+  }, []);
+
   return (
     <div>
       <h1>Bienvenido a la página principal</h1>
@@ -14,8 +35,31 @@ const HomePage = ({ user }) => {
       ) : (
         <p>Inicia sesión para ver el contenido.</p>
       )}
+
+      <h2>Videojuegos disponibles:</h2>
+      <ul>
+        {videojuegos.map(videojuego => (
+          <li key={videojuego.ID}>
+            <strong>Título:</strong> {videojuego.Título}<br />
+            <strong>Género:</strong> {videojuego.Género}<br />
+            <strong>Descripción:</strong> {videojuego.Descripción}
+          </li>
+        ))}
+      </ul>
+
+      <h2>Clubes disponibles:</h2>
+      <ul>
+        {clubes.map(club => (
+          <li key={club.ID}>
+            <strong>Nombre:</strong> {club.Nombre}<br />
+            <strong>Descripción:</strong> {club.Descripción}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
 
 export default HomePage;
+
+
